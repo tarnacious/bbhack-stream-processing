3b0a6d33bdc324efa0e7121184e17e026d7dfc57
Tarn Barford <tarn@tarnbarford.net>
2014-05-28 13:53:06 +0200

    Date fix for Hagens local

--- src/stream_processing/tweet.clj
+++ src/stream_processing/tweet.clj
@@ -1,10 +1,23 @@
 (ns stream-processing.tweet
+  (:use [clj-time.core]
+     [clj-time.format]
+     [clj-time.coerce])
   (:require [clojure.data.json :as json]
             [clj-time.format :as f]
-            ))
 
-(defn parse-date [s]
-  (f/parse (f/formatter "E MMM dd HH:mm:ss Z YYYY") s))
+            )
+  (:import java.util.Locale)
+  )
+
+(def twitter_formatter (with-locale (clj-time.format/formatter "EEE MMM dd HH:mm:ss Z yyyy") Locale/ENGLISH))
+(defn parse-date [data_string]
+  (try
+    (clj-time.coerce/to-string (clj-time.format/parse
+      twitter_formatter
+      data_string))
+  (catch Exception e
+     (println "Date conversion failed:" e)
+     "1970-01-01T00:00:01Z")))
 
 (defn parse-tweet [json]
   (let [tweet (json/read-str json)
